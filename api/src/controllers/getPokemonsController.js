060331dface4c7b7e892cd4611dd169e786430e2
elhombrej <94816708+elhombrej@users.noreply.github.com>
2022-12-08 16:21:56 -0300

    deployv11

--- api/src/controllers/getPokemonsController.js
+++ api/src/controllers/getPokemonsController.js
@@ -25,7 +25,7 @@ const getApiInfo = async ()=>{
         })
         pokemons.push(...auxPokemons);
         apiUrl = pokemonsApi.next;
-    }while(apiUrl != null && pokemons.length < 20);
+    }while(apiUrl != null && pokemons.length < 100);
 
     //console.log(pokemons);
 
